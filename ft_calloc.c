4d79acd4b074015bef8d1eb68cb31f598a7fbae2
Carlos Melero <carlos@carlosmelero.com>
2026-01-14 22:04:04 +0100

    calloc debe devolver puntero no null

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,17 @@
 /*   By: cmelero- <cmelero-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 17:37:42 by cmelero-          #+#    #+#             */
-/*   Updated: 2026/01/12 15:21:31 by cmelero-         ###   ########.fr       */
+/*   Updated: 2026/01/14 22:01:55 by cmelero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*Dependiendo de tu sistema operativo actual, el comportamiento de la
+función calloc puede diferir de lo descrito en su página del manual.
+Debes seguir la siguiente regla: Si nmemb o size es 0, entonces
+calloc() debe devolver un puntero único que pueda pasarse con éxito a
+free().*/
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*out;
